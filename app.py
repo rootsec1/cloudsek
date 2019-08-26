8634648efa646a2cd9207af165ed12c1cad8e946
abhishekwl <abhishekwl@protonmail.com>
2019-08-27 00:16:22 +0530

    Added /file endpoint and made /download accept only POST requests

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from multiprocessing import Process, Pool
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import pyrebase
 import requests
 import uuid
+import time
 
 firebaseConfig = {
     "apiKey": "AIzaSyDo5jCdYbKahPl4hekGz5QzEQwoaTkFIbU",
@@ -18,19 +19,24 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 
+CHUNK_SIZE = 8192
+
 def download_file_from_url(url, id):
     local_filename = url.split('/')[-1]
     with requests.get(url, stream=True) as r:
         size = int(r.headers.get('content-length'))
-        db.child(id).set({ 'size': size, 'done': 0 })
+        db.child(id).set({ 'name': local_filename, 'size': size, 'done': 0 })
         if r.status_code==200:
             with open(local_filename, 'wb') as f:
                 dl=0
-                for chunk in r.iter_content(chunk_size=8192):
+                start_time = int(time.time())
+                for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+                    end_time = int(time.time()) - start_time
                     f.write(chunk)
                     dl+=len(chunk)
                     if (dl*100/size)%10==0:
                         db.child(id).update({ 'done': dl })
+                    start_time = end_time
 
 app = Flask(__name__)
 limiter = Limiter(
@@ -39,7 +45,7 @@ limiter = Limiter(
     default_limits=["10 per minute"],
 )
 
-@app.route('/download')
+@app.route('/download', methods=['POST'])
 def download():
     remote_url = request.args.get('url')
     download_id = str(uuid.uuid4()).replace('-','')
@@ -51,9 +57,16 @@ def download():
 def status():
     download_id = request.args.get('id')
     obj = (db.child(download_id).get()).val()
-    obj['remaining'] = obj['size'] - obj['done']
+    obj['remaining'] = obj['size']-obj['done']
     obj['status'] = 'completed' if obj['remaining']==0 else 'downloading'
     return jsonify(obj)
+
+@app.route('/file')
+def get_file():
+    download_id = request.args.get('id')
+    obj = (db.child(download_id).get()).val()
+    local_filename = obj['name']
+    return send_file(local_filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
